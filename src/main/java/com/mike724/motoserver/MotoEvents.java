9aadcbe9096e65bbc5fbfbafecf066571e79aedd
Dakota <dakota@dasigns.net>
2013-08-14 12:33:04 -0400

    Use the latest build of MotoAPI

--- src/main/java/com/mike724/motoserver/MotoEvents.java
+++ src/main/java/com/mike724/motoserver/MotoEvents.java
@@ -1,13 +1,66 @@
 package com.mike724.motoserver;
 
 import com.mike724.motoapi.push.MotoPushEvent;
+import com.mike724.motoapi.storage.DataStorage;
+import com.mike724.motoapi.storage.defaults.NetworkPlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
 
 public class MotoEvents implements Listener {
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerLogin(PlayerLoginEvent e) {
+        //Set player to online and add them to the networkPlayers list
+        String playerName = e.getPlayer().getName();
+        DataStorage ds = MotoServer.getInstance().getDataStorage();
+
+        NetworkPlayer np = (NetworkPlayer) ds.getObject(NetworkPlayer.class, playerName);
+
+        if(np == null) {
+            np = new NetworkPlayer(playerName);
+        } else if(np.isOnline()) {
+            e.setResult(PlayerLoginEvent.Result.KICK_FULL);
+            e.setKickMessage("You are already logged in to another server!");
+        }
+
+        np.setOnline(true);
+        ds.writeObject(np,playerName);
+
+        MotoServer.getInstance().addNetworkPlayer(np);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        //Set player to offline and remove them from the networkPlayers list
+        String playerName = e.getPlayer().getName();
+        DataStorage ds = MotoServer.getInstance().getDataStorage();
+
+        NetworkPlayer np = MotoServer.getInstance().getNetworkPlayers().get(playerName);
+        np.setOnline(false);
+        ds.writeObject(np,playerName);
+
+        MotoServer.getInstance().removeNetworkPlayer(np.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerQuit(PlayerKickEvent e) {
+        //Set player to offline and remove them from the networkPlayers list if they are on it
+        String playerName = e.getPlayer().getName();
+        if(MotoServer.getInstance().getNetworkPlayers().containsKey(playerName)) {
+            DataStorage ds = MotoServer.getInstance().getDataStorage();
+
+            NetworkPlayer np = MotoServer.getInstance().getNetworkPlayers().get(playerName);
+            np.setOnline(false);
+            ds.writeObject(np,playerName);
+
+            MotoServer.getInstance().removeNetworkPlayer(np.getPlayer());
+        }
+    }
+
 
     //Parse network events
     @EventHandler(priority = EventPriority.MONITOR)
--- src/main/java/com/mike724/motoserver/MotoServer.java
+++ src/main/java/com/mike724/motoserver/MotoServer.java
@@ -1,9 +1,15 @@
 package com.mike724.motoserver;
 
 import com.mike724.motoapi.push.MotoPush;
+import com.mike724.motoapi.storage.DataStorage;
+import com.mike724.motoapi.storage.defaults.NetworkPlayer;
+import org.apache.commons.io.IOUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
 
 public class MotoServer extends JavaPlugin {
 
@@ -11,17 +17,38 @@ public class MotoServer extends JavaPlugin {
 
     private MotoPush motoPush;
 
+    private HashMap<String,NetworkPlayer> networkPlayers = new HashMap<>();
+
+    private String externalIP;
+
 	@Override
 	public void onEnable() {
+        //Set our instance
         instance = this;
 
+        //Get this servers external ip
+        InputStream is = null;
+        try {
+            is = new URL("http://checkip.amazonaws.com/").openStream();
+            this.externalIP = IOUtils.toString(is) + ":" + this.getServer().getPort();
+        } catch (IOException e) {
+            System.out.println("Couldn't get external IP.. stopping");
+            this.getServer().shutdown();
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+
+        //Setup MotoPush
         try {
             motoPush = new MotoPush();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        //Setup the event listener
         getServer().getPluginManager().registerEvents(new MotoEvents(), this);
+
+        //Setup the network command listener
         getCommand("net").setExecutor(new MotoCommands(this));
 
         this.getLogger().info("MotoServer Enabled");
@@ -29,11 +56,43 @@ public class MotoServer extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
-		this.getLogger().info("MotoServer Disabled");
+        //Set all players to offline when the server goes down
+        HashMap<Object,String> playerObjects = new HashMap<>();
+        for(NetworkPlayer np : networkPlayers.values()) {
+            np.setOnline(false);
+            playerObjects.put((Object) np, np.getPlayer());
+        }
+        this.getDataStorage().writeObjects(playerObjects);
+
+        //Uh, oh
+        this.getLogger().info("MotoServer Disabled");
+
+        //If we go down, the server does too!!
+        this.getServer().shutdown();
 	}
 
     public MotoPush getMotoPush() {
         return motoPush;
+    }
+
+    public DataStorage getDataStorage() {
+        return null;
+    }
+
+    protected void addNetworkPlayer(NetworkPlayer np) {
+        networkPlayers.put(np.getPlayer(),np);
+    }
+
+    protected void removeNetworkPlayer(String p) {
+        networkPlayers.remove(p);
+    }
+
+    public HashMap<String,NetworkPlayer> getNetworkPlayers() {
+        return networkPlayers;
+    }
+
+    public String getExternalIP() {
+        return externalIP;
     }
 
     public static MotoServer getInstance() {
